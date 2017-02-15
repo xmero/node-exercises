26acfa9aab3db930f097ffd79d1c3d6b0eb5c8d5
Xavier Meroño <x.merono@gmail.com>
2017-02-15 11:44:15 +0100

    exercise 14 updated

--- 14json-object/main.js
+++ 14json-object/main.js
@@ -1,17 +1,22 @@
 var fs = require('fs')
 
 
-var obj
+var data = require("./user.json")
+    console.log( data)
+ obj = JSON.parse(fs.readFileSync('./user.json', 'utf8'))
 
-function getJson() {
-    fs.readFile('user.json', 'utf8', function(err, data) {
-        if (err) throw err
-        obj = JSON.parse(data)
-        console.log('the username is ==>'+obj.username)
-        console.log(obj)
-    })
-}
+       // console.log('the username is ==>'+obj[1].username)
+    console.log(obj)
 
-getJson()
+// function getJson() {
+//     fs.readFile('user.json', 'utf8', function(err, data) {
+//         if (err) throw err
+//         obj = JSON.parse(data)
+//         console.log('the username is ==>'+obj.username)
+//         console.log(obj)
+//     })
+// }
+
+// getJson()
 
 
